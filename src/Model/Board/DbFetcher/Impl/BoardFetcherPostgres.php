cb79b5f2fcc43f692c5f588f55e70f6318d06ff3
EugeneSamoylov home <evgeny4815@gmail.com>
2021-04-01 22:52:58 +0500

    Refactoring query in BoardFetcherPostgres.php

--- src/Model/Board/DbFetcher/Impl/BoardFetcherPostgres.php
+++ src/Model/Board/DbFetcher/Impl/BoardFetcherPostgres.php
@@ -44,35 +44,31 @@ class BoardFetcherPostgres implements  IBoardFetcher
     protected function getBoardsWithMessagePerHourRate(bool $sortDesc = true): string
     {
         return "SELECT query.name,
-                   query.path,
-                   CASE
-                       WHEN query.countOfHours=0
-                            OR query.countOfHours IS NULL
-                            OR query.messengesOnThreadCount IS NULL THEN 0
-                       WHEN query.countOfHours < 1 THEN query.messengesOnThreadCount 
-                       ELSE round((query.messengesOnThreadCount / query.countOfHours)::numeric, 2)
-                   END AS postPerHour
-            FROM
-              (SELECT b.name AS name,
-                      b.path AS PATH,
-                      EXTRACT(epoch
-                             --FROM (m.lastMessageOnBoardTime - tr.oldestThreadCreationTime))/3600 AS countOfHours,
-                               FROM (:currentTime - tr.oldestThreadCreationTime))/3600 AS countOfHours,
-                      m.messengesOnThreadCount AS messengesOnThreadCount
-               FROM board AS b
-               LEFT JOIN
-                 (SELECT min(t.creation_time) AS oldestThreadCreationTime,
-                         t.board_id
-                  FROM THREAD AS t
-                  GROUP BY t.board_id) AS tr ON tr.board_id = b.id
-               LEFT JOIN
-                 (SELECT max(m.time) AS lastMessageOnBoardTime,
-                         count(m.id) AS messengesOnThreadCount,
-                         t.board_id
-                  FROM THREAD AS t
-                  LEFT JOIN message m ON t.id = m.thread_id
-                  GROUP BY t.board_id) AS m ON m.board_id = b.id
-               WHERE LOWER(b.name) LIKE '%'||:name||'%' ) AS query
-               ORDER BY postPerHour " . ($sortDesc ? "DESC;" : "ASC;");
+                       query.path,
+                       CASE
+                           WHEN query.countOfHours=0
+                                OR query.countOfHours IS NULL
+                                OR query.messengesOnThreadCount IS NULL THEN 0
+                           WHEN query.countOfHours < 1 THEN query.messengesOnThreadCount
+                           ELSE round((query.messengesOnThreadCount / query.countOfHours)::numeric, 2)
+                       END AS postPerHour
+                FROM
+                  (SELECT b.name AS name,
+                          b.path AS PATH,
+                          EXTRACT(epoch --FROM (tr.lastMessageOnBoardTime - tr.oldestThreadCreationTime))/3600 AS countOfHours,
+                
+                                  FROM (:currentTime - tr.oldestThreadCreationTime))/3600 AS countOfHours,
+                          tr.messengesOnThreadCount AS messengesOnThreadCount
+                   FROM board AS b
+                   LEFT JOIN
+                     (SELECT min(t.creation_time) AS oldestThreadCreationTime,
+                             max(m.time) AS lastMessageOnBoardTime,
+                             count(m.id) AS messengesOnThreadCount,
+                             t.board_id
+                      FROM THREAD AS t
+                      LEFT JOIN message m ON t.id = m.thread_id
+                      GROUP BY t.board_id) AS tr ON tr.board_id = b.id
+                   WHERE LOWER(b.name) LIKE '%'||:name||'%' ) AS query
+                ORDER BY postPerHour " . ($sortDesc ? "DESC;" : "ASC;");
     }
 }
